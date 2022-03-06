01f31cc9320af1ddc9fedac28d6ac8d8b4fd51f8
Sanghyeok Park <tkdgur159@gmail.com>
2022-03-06 22:05:50 +0900

    first commit

---
+++ variable.js
@@ -0,0 +1,18 @@
+//1. Use strict
+
+'use strict';
+
+//2. Variable
+//let (added in ES6)
+
+let name = 'ellie';
+console.log(name);
+name = 'hello';
+console.log(name);
+
+
+
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
